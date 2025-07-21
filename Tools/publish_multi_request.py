c63d4533318132a4afbbb439d9a2f7a5ba0c89a5
Andrey Ushakov <10843461+Werzet@users.noreply.github.com>
2025-07-21 19:10:58 +1000

    Update cdn address

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://lk.ss220.club/cdn/"
+ROBUST_CDN_URL = "https://cdn.ss220.club/"
 FORK_ID = "SS220MasterStation"
 
 def main():
